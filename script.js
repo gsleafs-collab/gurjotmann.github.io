f480593aae083f99a1cf28526081f2775bc10fc6
gsleafs-collab <g.s.leafs@gmail.com>
2025-08-10 11:25:31 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-// Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
-  });
-});
